ca3f09bde903360298af38edb13d1cd648d500c5
BobJiang <374123140@qq.com>
2023-07-26 23:36:28 +0800

    Sprint.go

---
+++ ch12/diy_sprint/Sprint.go
@@ -0,0 +1,29 @@
+package main
+
+import "strconv"
+
+func Sprint(x interface{}) string {
+	type stringer interface {
+		String() string
+	}
+	switch x := x.(type) {
+	case stringer:
+		return x.String()
+	case string:
+		return x
+	case int:
+		return strconv.Itoa(x)
+	case bool:
+		if x {
+			return "true"
+		}
+		return "false"
+	default:
+		//array、chan、func、map、pointer、slice、struct
+		return "???"
+	}
+}
+
+func main() {
+
+}
