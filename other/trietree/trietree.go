c62838e05b173b087e2391f2bad3d76c830d9540
bobJiang <374123140@qq.com>
2024-02-26 10:03:39 +0800

    修改文件夹名，添加前缀树

---
+++ other/trietree/trietree.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+type Trie struct {
+	children [26]*Trie
+	isEnd    bool
+}
+
+func Constructor() Trie {
+	return Trie{}
+}
+
+func (t *Trie) Insert(word string) {
+	node := t
+	for _, ch := range word {
+		ch -= 'a'
+		if node.children[ch] == nil {
+			node.children[ch] = &Trie{}
+		}
+		node = node.children[ch]
+	}
+}
+
+func (t *Trie) SearchPrefix(prefix string) *Trie {
+	node := t
+	for _, ch := range prefix {
+		ch -= 'a'
+		if node.children[ch] == nil {
+			return nil
+		}
+		node = node.children[ch]
+	}
+	return node
+}
+
+func (t *Trie) Search(word string) bool {
+	node := t.SearchPrefix(word)
+	return node != nil && node.isEnd
+}
+
+func (t *Trie) StartsWith(prefix string) bool {
+	return t.SearchPrefix(prefix) != nil
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Insert(word);
+ * param_2 := obj.Search(word);
+ * param_3 := obj.StartsWith(prefix);
+ */
+
+func main() {
+	trie := Constructor()
+	trie.Insert("apple")
+	fmt.Println(trie.Search("apple"))   // 返回 True
+	fmt.Println(trie.Search("app"))     // 返回 False
+	fmt.Println(trie.StartsWith("app")) // 返回 True
+	trie.Insert("app")
+	fmt.Println(trie.Search("app")) // 返回 True
+}
